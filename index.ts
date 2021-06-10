b1950e37b88798a21114125bd48019610ea3ee10
Anwesh43 <anweshthecool0@gmail.com>
2021-06-10 15:19:51 +0530

    adding LSSNode class

--- index.ts
+++ index.ts
@@ -141,3 +141,45 @@ class Animator {
         }
     }
 }
+
+class LSSNode {
+
+    state : State = new State()
+    prev : LSSNode 
+    next : LSSNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new LSSNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawLSNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LSSNode {
+        var curr : LSSNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
